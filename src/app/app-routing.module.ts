55c9869bcab6ae51eaa19329f1c9cec4ee3aff46
Deepak <satulurideepak4@gmail.com>
2021-06-24 13:10:12 +0530

    Login and Signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserSignInComponent} from './user/user-sign-in/user-sign-in.component';
+import {UserSignUpComponent} from './user/user-sign-up/user-sign-up.component';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: UserSignInComponent},
+  {path: 'signup', component: UserSignUpComponent},
+  {path: 'home', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule} from '@angular/material/card';
+import { UserSignInComponent } from './user/user-sign-in/user-sign-in.component';
+import { UserSignUpComponent } from './user/user-sign-up/user-sign-up.component';
+import { HomeComponent } from './home/home.component';
+
+
+const materialComponents = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatCardModule
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSignInComponent,
+    UserSignUpComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    materialComponents,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
