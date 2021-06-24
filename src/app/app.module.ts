fd6197a27fdaff5cc1e0485107e70315a0eebc8a
kasyap23 <kasyapgundu2001@gmail.com>
2021-06-24 14:16:29 +0530

    Product,Users,Orders component initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/users/UserSignUpFormComponent/signup.component';
+import { SigninComponent } from './components/users/UserSignInFormComponent/signin.component';
+import { ViewUserComponent } from './components/users/ViewUserProfileComponent/view-user.component';
+import { EditUserComponent } from './components/users/EditUserProfileComponent/edit-user.component';
+import { ProductDetailsComponent } from './components/products/ProductDetailsComponent/product-details.component';
+import { OrderDetailsComponent } from './components/orders/OrderDetailsComponent/order-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/NavbarComponent/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    ViewUserComponent,
+    EditUserComponent,
+    ProductDetailsComponent,
+    OrderDetailsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
